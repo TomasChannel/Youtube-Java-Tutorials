d3e3b45f88cee2bbedb2e149a1c6a780fda2b996
Tomas <tclmendes@hotmail.com>
2019-03-21 11:31:37 +1300

    Update SourceCode.java

--- Source Code/#1 Creating Your First Ever Program/SourceCode.java
+++ Source Code/#1 Creating Your First Ever Program/SourceCode.java
@@ -1,1 +1,19 @@
-:)
+// one class needs to have a main() method
+public class HelloWorld{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args){
+    Staing name = "Tomas";
+    int age = 21;
+    System.out.print("Hello, my name is "+name+" and I am "+age+" 21 years old; 
+      
+  }
+}
+                     
+// TO COMPILE do javac HelloWorld.java
+// TO RUN do     java HelloWorld
+// Make sure that the file name is HelloWorld.java
+// to navigate through cmd on windows you can use:
+                     // cd ..
+                     // cd DIRECTORY NAME
+                     // dir
+                     // more info on cmd https://skimfeed.com/blog/windows-command-prompt-ls-equivalent-dir/
